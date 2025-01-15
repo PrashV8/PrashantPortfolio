52052d854a230f5fbf33eced259ae89307bf9076
PrashV8 <prashantvr8@gmail.com>
2025-01-16 00:21:27 +0530

    first commit

---
+++ data/index.ts
@@ -0,0 +1,241 @@
+export const navItems = [
+    { name: "About", link: "#about" },
+    { name: "Projects", link: "#projects" },
+    { name: "Testimonials", link: "#testimonials" },
+    { name: "Contact", link: "#contact" },
+  ];
+  
+  export const gridItems = [
+    {
+      id: 1,
+      title: "I prioritize client collaboration and foster open communication, ensuring a seamless and productive partnership.",
+      description: "",
+      className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+      imgClassName: "w-full h-full",
+      titleClassName: "justify-end",
+      img: "/b1.svg",
+      spareImg: "",
+    },
+    {
+      id: 2,
+      title: "I'm very flexible with time zone communications",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+      imgClassName: "",
+      titleClassName: "justify-start",
+      img: "",
+      spareImg: "",
+    },
+    {
+      id: 3,
+      title: "My tech stack",
+      description: "I constantly try to improve",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+      imgClassName: "",
+      titleClassName: "justify-center",
+      img: "",
+      spareImg: "",
+    },
+    {
+      id: 4,
+      title: "Tech enthusiast with a passion for innovative development.",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+      imgClassName: "",
+      titleClassName: "justify-start",
+      img: "/grid.svg",
+      spareImg: "/b4.svg",
+    },
+  
+    {
+      id: 5,
+      title: "Currently building a Web Application for health AI",
+      description: "The Inside Scoop",
+      className: "md:col-span-3 md:row-span-2",
+      imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+      titleClassName: "justify-center md:justify-start lg:justify-center",
+      img: "/b5.svg",
+      spareImg: "/grid.svg",
+    },
+    {
+      id: 6,
+      title: "Do you want to hire me or start a project together?",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+      imgClassName: "",
+      titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+      img: "",
+      spareImg: "",
+    },
+  ];
+  
+  export const projects = [
+    {
+      id: 1,
+      title: "JobBazaar",
+      des: "JobMarket: A React Native app for job seekers, offering features like job search, filters, saved listings, and application tracking.",
+      img: "/vds.png",
+      iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
+      link: "https://github.com/PrashV8/job-market",
+    },
+    {
+      id: 2,
+      title: "Fake News Detection System",
+      des: "Fake News Detection System: Utilizing AI, it analyzes content to identify misinformation, empowering users with reliable information using Python.",
+      img: "/designer.jpeg",
+      iconLists: ["/python.svg", "/pytorch.svg", "/job.svg", "/ai.svg", "/algorithm.svg"],
+      link: "https://github.com/PrashV8/Fake-news-detection-System",
+    },
+    {
+      
+        id: 4,
+        title: "Task Management Scheduler",
+        des: "A React Native app designed for Android and iOS, providing users with tools to efficiently schedule tasks, set reminders, and manage their daily activities with an intuitive interface.",
+        img: "/task-manager.png", // Update the image path accordingly
+        iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/next.svg",], // Icons relevant to the project
+        link: "https://github.com/PrashV8/Task-Manager", // Replace with your actual GitHub link
+      
+      
+    
+    },
+    {
+      id: 4,
+      title: "Traffic Signs Detection",
+      des: "Developed a system that detects and converts traffic signs from over 45 countries into real-time audio alerts using text-to-speech, resulting in a 30% reduction in driver distractions and improving road safety.",
+      img: "/traff.jpeg",
+      iconLists: ["/python.svg", "/pytorch.svg", "/job.svg", "/ai.svg", "/algorithm.svg"],
+      link: "https://github.com/PrashV8/Traffic-Sign-Detection",
+    },
+  ];
+  
+  export const testimonials = [
+    {
+      quote:
+       "Collaborating with Prashant was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Prashant's enthusiasm for every aspect of web development, especially in crafting the best user interfaces, truly stands out. If you're seeking to elevate your website and enhance your brand, Prashant is the ideal partner.",
+      name: "Nitish Kumar",
+      title: "Friend",
+    },    {
+      quote:
+       `Prashant is driven by a passion for creating solutions that make a big impact. Whether it's developing innovative web applications or leading a team to success, he always strives to deliver results that go beyond expectations. His commitment to excellence and ability to think strategically enable him to tackle complex challenges and turn them into opportunities for growth and improvement. Prashant's work is not just about completing tasks—it's about making a meaningful difference, ensuring that every project he touches leaves a lasting, positive impact.` ,
+      name: "Prakshal Jain",
+      title: "Major Project Member",
+    },
+    {
+      quote:
+        "Working with Prashant highlighted his exceptional management skills and strong leadership qualities. He has an innate ability to communicate effectively, ensuring that all team members are aligned and motivated towards a common goal. Prashant's leadership is marked by his proactive approach and his talent for fostering collaboration, making every team project a seamless and productive experience. His capacity to balance technical expertise with clear, empathetic communication sets him apart as a leader who not only drives success but also inspires those around him.",
+      name: "Sharanya Raj",
+      title: "Co-Founder of PAA",
+    },
+    {
+      quote:
+        ` Prashant's expertise in web application development is truly remarkable. He has a deep understanding of both front-end and back-end technologies, allowing him to build robust, scalable, and user-friendly applications. His attention to detail ensures that every aspect of the web app is optimized for performance and usability. Prashant's ability to integrate complex functionalities seamlessly while maintaining a sleek, intuitive user interface makes him an invaluable asset to any development project. If you are looking to bring your web app ideas to life with precision and creativity, Prashant is the developer you need.`,
+      name: "Rishabh Nigam",
+      title: "Major Project Member",
+    },
+
+    {
+      quote:
+        ` Prashant is driven by a passion for creating solutions that make a big impact. Whether it's developing innovative web applications or leading a team to success, he always strives to deliver results that go beyond expectations. His commitment to excellence and ability to think strategically enable him to tackle complex challenges and turn them into opportunities for growth and improvement. Prashant's work is not just about completing tasks—it's about making a meaningful difference, ensuring that every project he touches leaves a lasting, positive impact.`,
+      name: "Prakhar tondon",
+      title: "Friend",
+    },
+  ];
+  
+  export const companies = [
+    {
+      id: 1,
+      name: "cloudinary",
+      img: "/cpp.svg",
+      nameImg: "/cpp.svg",
+    },
+    {
+      id: 2,
+      name: "appwrite",
+      img: "/app.svg",
+      nameImg: "/nextjs.svg",
+    },
+    {
+      id: 3,
+      name: "HOSTINGER",
+      img: "/host.svg",
+      nameImg: "/javascript.svg",
+    },
+    {
+      id: 4,
+      name: "stream",
+      img: "/s.svg",
+      nameImg: "/mongodb.svg",
+    },
+    {
+      id: 5,
+      name: "docker.",
+      img: "/dock.svg",
+      nameImg: "/python.svg",
+    },
+    {
+      id: 6,
+      name: "docker.",
+      img: "/dock.svg",
+      nameImg: "/tail.svg",
+    },    {
+      id: 7,
+      name: "docker.",
+      img: "/dock.svg",
+      nameImg: "/re.svg",
+    }, {
+      id: 8,
+      name: "docker.",
+      img: "/dock.svg",
+      nameImg: "/nodejs.svg",
+    }
+  ];
+  
+  export const workExperience = [
+    {
+      id: 1,
+      title: "DEV Support",
+      desc: "Assisted in the development of a web-based platform using ReactJS and NextJS, enhancing interactivity.",
+      className: "md:col-span-2",
+      thumbnail: "/exp1.svg",
+    },
+    {
+      id: 2,
+      title: "Mobile App Dev",
+      desc: "Designed and developed mobile app for both iOS & Android platforms using React Native.",
+      className: "md:col-span-2", // change to md:col-span-2
+      thumbnail: "/exp2.svg",
+    },
+
+    {
+      id: 3,
+      title: "Co-Founder - PAA & FUNDA",
+      desc: "The Protect Abandoned Animals Foundation (PAA) has rescued and cared for dogs and cats. In FUNDA, We provide support and resources to help young people combat social media bullying, depression, and anxiety.",
+      className: "md:col-span-2", // change to md:col-span-2
+      thumbnail: "/exp3.svg",
+    },
+    {
+      id: 4,
+      title: "Web Developer - Anvi",
+      desc: "Anvi is a startup focused on cryptocurrency exchange, offering a secure platform for trading various cryptocurrencies. I assisted in developing user-facing features using modern frontend technologies.",
+      className: "md:col-span-2",
+      thumbnail: "/exp4.svg",
+    },
+  ];
+  
+  export const socialMedia = [
+    {
+      id: 1,
+      img: "/git.svg",
+      link: "https://github.com/PrashV8",
+    },
+    {
+      id: 2,
+      img: "/twit.svg",
+      link: "https://x.com/prash4ant",
+    },
+    {
+      id: 3,
+      img: "/link.svg",
+      link: "https://www.linkedin.com/in/prashant-verma-104643210/",
+    },
+  ];
